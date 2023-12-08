c77b658c5ce9605723672087c7bdac9aa961804e
Ata Fatahi <immrata@gmail.com>
2023-12-07 23:38:28 -0800

    Use SIMD instructions for MatMul on CPU
    Signed-off-by: Ata Fatahi <immrata@gmail.com>

--- src/device/cpu.rs
+++ src/device/cpu.rs
@@ -1,27 +1,32 @@
 use rayon::prelude::*;
+use packed_simd::*;
 
 use super::device::*;
 pub struct CPU {}
 impl Device for CPU {
     type Err = ();
     fn matmul(o: &mut [f32], a: &[f32], b: &[f32], width: usize, _o_rows: usize, o_cols: usize) -> Result<(), ()> {
+        if width % f32x4::lanes() != 0 || o_cols % f32x4::lanes() != 0 {
+            return Err(()); // Return error if matrix dimensions are not divisible by SIMD lanes
+        }
 
-        o.par_iter_mut().enumerate().for_each(
-            |(idx, o)| {
-                let r = idx / o_cols;
-                let c = idx % o_cols;
-                let mut v = 0.0;
-                for k in 0..width {
-                    v += a[r * width + k] * b[k * o_cols + c];
+        let a_chunks = a.chunks_exact(width);
+        let b_chunks = b.chunks_exact(o_cols);
+
+        for (i, a_chunk) in a_chunks.enumerate() {
+            let o_start = i * o_cols;
+            for (j, b_chunk) in b_chunks.clone().enumerate() {
+                let mut v = f32x4::splat(0.0);
+                for k in (0..width).step_by(f32x4::lanes()) {
+                    let a_simd = f32x4::from_slice_unaligned(&a_chunk[k..]);
+                    let b_simd = f32x4::from_slice_unaligned(&b_chunk[j * o_cols + k..]);
+                    v += a_simd * b_simd;
                 }
-                *o = v;
-
+                v.write_to_slice_unaligned(&mut o[o_start + j * f32x4::lanes()..]);
             }
-
-        );
+        }
 
         Ok(())
-
     }
 }
 
